7e22b6e99d10f6840cc43e8e0f6f9ff3364e2695
Muzahidul Islam Saki <muzahidulislamsaki@gmail.com>
2025-08-02 08:08:38 +0600

    lab 3 continue

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,23 @@ import { AppService } from './app.service';
 import { AdminController } from './/admin/admin.controller';
 import { UserModule } from './user/user.module';
 import { BusOwnerModule } from './bus_owner/bus_owner.module';
-import { dataSourceOptions } from 'db/data-source';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+config();
 
 @Module({
-  imports: [UserModule, BusOwnerModule],
+  imports: [UserModule, BusOwnerModule, TypeOrmModule.forRoot(
+    {
+      type: 'postgres',
+      host: process.env.DB_HOST ,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    } 
+  )],
   controllers: [AppController, AdminController],
   providers: [AppService],
 })
